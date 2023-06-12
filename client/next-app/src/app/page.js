ccd9741b226579fc23022129418f091ab7bac1bb
dna737 <adnanhere27@gmail.com>
2023-06-12 17:31:17 -0500

    attempt to create main article section of the home page

--- client/next-app/src/app/page.js
+++ client/next-app/src/app/page.js
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import NavBar from './components/ui/NavBar'
+import Image from "next/image";
+import Link from "next/link";
+import NavBar from "./components/ui/NavBar";
+import MainArticle from "./components/MainArticle";
 
 export default function Home() {
   return (
     <main>
-      <NavBar></NavBar>
+      <NavBar />
+      <MainArticle />
     </main>
-    
-  )
+  );
 }
