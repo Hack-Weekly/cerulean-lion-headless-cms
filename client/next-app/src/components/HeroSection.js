990abd2a81985484b827f4c13ad4c71513811cc3
Dedios <dediosprogramming@gmail.com>
2023-06-13 00:58:26 -0400

    HeroSection component created

---
+++ client/next-app/src/components/HeroSection.js
@@ -0,0 +1,23 @@
+import background from "@/image/home-page-lion.jpg";
+
+export default function HeroSection() {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
