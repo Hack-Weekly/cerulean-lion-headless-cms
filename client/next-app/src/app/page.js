d012ac5b0513622c4c7bde8cf1d30be5582dfab6
Dedios <dediosprogramming@gmail.com>
2023-06-13 00:56:38 -0400

    added HeroSection component

--- client/next-app/src/app/page.js
+++ client/next-app/src/app/page.js
@@ -4,20 +4,18 @@ import Link from "next/link";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/NavBar";
 import MainArticle from "@/components/MainArticle";
+import HeroSection from "@/components/HeroSection";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={"columns-3 `${inter.className}`"}>
+    <main className={"columns-2 `${inter.className}`"}>
       <div>
         <NavBar />
       </div>
       <div>
-        <h1>hello</h1>
-      </div>
-      <div>
-        <h1>hello</h1>
+        <HeroSection />
       </div>
     </main>
   );
