df2da17295f532fb09a64a61f85c059ba81e7c4b
Dedios <dediosprogramming@gmail.com>
2023-06-14 14:25:25 -0400

    NavBar refactored

--- client/next-app/src/components/NavBar.js
+++ client/next-app/src/components/NavBar.js
@@ -14,12 +14,12 @@ export default function NavBar() {
       <h1 className="title-container font-heading mb-5 text-5xl text-amber-500 drop-shadow-lg">
         Cerulean Lion
       </h1>
-      <div className="font-general text-white">
+      <div className="font-general text-white ">
         <Link href="/">Home</Link>
-        <Link href="/blog" className="navbar-links">
+        <Link href="/blog" className="block">
           Blog
         </Link>
-        <Button text={loginButton} />
+        <Link href="/login">Login</Link>
       </div>
     </div>
   );
