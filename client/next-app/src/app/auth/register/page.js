3aedfc2f2bb8394a17d630ada420b144915e8797
Dedios <dediosprogramming@gmail.com>
2023-06-14 16:16:48 -0400

    refactored auth route Page & Register component

--- client/next-app/src/app/auth/register/page.js
+++ client/next-app/src/app/auth/register/page.js
@@ -5,7 +5,6 @@ import Register from "@/components/auth/Register";
 export default function Page() {
   return (
     <div>
-      <h1>Hello World</h1>
       <Register />
     </div>
   );
--- client/next-app/src/components/auth/Register.js
+++ client/next-app/src/components/auth/Register.js
@@ -4,7 +4,7 @@ export default function Register() {
       <section>
         <div class="w-full lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
           <h2 class="text-2xl font-bold text-gray-900 dark:text-white">
-            Sign in to Flowbite
+            Get a Cerulean Lion Account 🦁
           </h2>
           <form class="mt-8 space-y-6" action="#">
             <div>
@@ -12,14 +12,14 @@ export default function Register() {
                 for="email"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Your email
+                Email
               </label>
               <input
                 type="email"
                 name="email"
                 id="email"
                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="name@company.com"
+                placeholder="name@mail.com"
                 required
               />
             </div>
@@ -28,7 +28,7 @@ export default function Register() {
                 for="password"
                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Your password
+                Password
               </label>
               <input
                 type="password"
@@ -69,12 +69,12 @@ export default function Register() {
               type="submit"
               class="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Login to your account
+              Sign Up!
             </button>
             <div class="text-sm font-medium text-gray-900 dark:text-white">
-              Not registered yet?{" "}
+              Have an account?{" "}
               <a class="text-blue-600 hover:underline dark:text-blue-500">
-                Create account
+                Login
               </a>
             </div>
           </form>
